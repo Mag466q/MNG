4dd1d01dfe3549a8dbdbc84008e3c4eeec7b84ee
Mag466q <77707803+Mag466q@users.noreply.github.com>
2022-11-10 16:51:15 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,63 +1,107 @@
 import csv
 import re
 
-ublisher_location = []
+separator = "$"
+name_csv = "new.csv"
+
+publisher_location = []
 publisher_name = []
 publisher_year = []
 no = []
 vol = []
 article_no = []
 pages_in_range = []
-separator = "$"
-name_csv = "new.csv"
-def fill_data(data):
+
+def fill_data(data:object) -> str:
     if data != None:
         return data.group()
     else:
-        return "NULL"
+        return " "
 
+def re_publisher_location(row:str):
+    buff = re.search("(?P<publisher_location>[a-zA-Z-żéźćńółęąśŻŹĆĄŚĘŁÓŃ]+\s)", row)
+    publisher_location.append(fill_data(buff))
 
-def test(tab):
-    good = 0
-    bad = 0
-    for data in tab:
-        if data == "NULL":
-            bad = bad + 1
-        else:
-            good = good + 1
+def re_publisher_name(row:str):
+    buff = re.search("(?P<publisher_name>([a-zA-Z-żéźćńółęąśŻŹĆĄŚĘŁÓŃ]+\s)*([(]([A-Z])*[)]|[a-zA-Z-éżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)(,|(\s\\\)))",row)
+    publisher_name.append(fill_data(buff))
 
-    print(str(good) + "/" + str(len(tab)) + " " + str(bad))
+def re_no(row:str):
+    buff = re.search("(?P<no>((nr|Nr)|(No|no)\.)\s\d{1,4})", row)
+    no.append(fill_data(buff))  # bez iss.
+
+def re_publisher_year(row:str):
+    buff = re.search("(?P<publisher_year>\d\d\d\d)", row)
+    publisher_year.append(fill_data(buff))
+
+def re_vol(row:str):
+    buff = re.search("(?P<vol>(vol|Vol)\.\s\d{1,3})", row)
+    vol.append(fill_data(buff))
+
+def re_article_no(row:str):
+    buff = re.search("(?P<article_no>[e]+\d\d\d\d\d)", row)
+    article_no.append(fill_data(buff))
+
+def re_pages_in_range(row:str):
+    buff = re.search("(?P<pages_in_range>(S\.|s\.)\s\d{1,9}[-]\d{1,9})", row)
+    pages_in_range.append(fill_data(buff))
+
+def clear():
+
+    buffer = publisher_name.copy()
+    publisher_name.clear()
+    for buff in buffer :
+        publisher_name.append((buff[:-1]))
+    buffer.clear()
+
+    buffer = no.copy()
+    no.clear()
+    for buff in buffer:
+        data_filter = re.search("\d{1,4}",buff)
+        no.append(fill_data(data_filter))
+    buffer.clear()
+
+    buffer = vol.copy()
+    vol.clear()
+    for buff in buffer:
+        data_filter = re.search("\d{1,3}",buff)
+        vol.append(fill_data(data_filter))
+    buffer.clear()
+
+    buffer = pages_in_range.copy()
+    pages_in_range.clear()
+    for buff in buffer:
+        data_filter = re.search("\d{1,9}[-]\d{1,9}", buff)
+        pages_in_range.append(fill_data(data_filter))
+    buffer.clear()
 
 
 with open('details.csv', 'r', encoding="utf8") as file:
     reader = csv.reader(file)
     for row in reader:
-        x = re.search("(?P<ublisher_location>[a-zA-Z-żźćńółęąśŻŹĆĄŚĘŁÓŃ]+\s)", row[0])
-        y = re.search("(?P<publisher_name>([a-zA-Z-żźćńółęąśŻŹĆĄŚĘŁÓŃ]+\s)*([(]([A-Z])*[)]|[a-zA-Z-żźćńółęąśŻŹĆĄŚĘŁÓŃ]+)(,|(\s\\\)))",row[0])
-        z = re.search("(?P<publisher_year>\d\d\d\d)", row[0])
-        v = re.search("(?P<no>((nr|Nr)|(No|no)\.)\s\d{1,4})",row[1])
-        t = re.search("(?P<vol>(vol|Vol)\.\s\d{1,3})",row[1])
-        u = re.search("(?P<article_no>[e]+\d\d\d\d\d)",row[1])
-        h = re.search("(?P<pages_in_range>(S\.|s\.)\s\d{1,9}[-]\d{1,9})",row[1])
-        ublisher_location.append(fill_data(x))
-        publisher_name.append(fill_data(y))
-        publisher_year.append(fill_data(z))
-        no.append(fill_data(v)) #bez iss.
-        vol.append(fill_data(t))
-        article_no.append(fill_data(u))
-        pages_in_range.append(fill_data(h))
+        re_publisher_location(row[0])
+        re_publisher_name(row[0])
+        re_publisher_year(row[0])
+        re_no(row[1])
+        re_vol(row[1])
+        re_article_no(row[1])
+        re_pages_in_range(row[1])
+
+clear()
+
+with open(name_csv,"w",encoding="utf8",newline="\n") as file:
+    file.write("publisher_location" + separator + "publisher_name" + separator + "publisher_year" + separator + "no" + separator + "vol" + separator + "article_no" + separator + "pages_in_range" + "\n")
+    for val in range(0,700):
+        file.write(publisher_location[val] + separator +
+                   publisher_name[val] + separator +
+                   publisher_year[val] + separator +
+                   no[val] + separator +
+                   vol[val] + separator +
+                   article_no[val] + separator +
+                   pages_in_range[val] +
+                "\n")
 
 
 
-test(ublisher_location)
-test(publisher_name)
-test(publisher_year)
-test(no)
-test(vol)
-test(article_no)
-test(pages_in_range)
-print(ublisher_location)
 
-with open(name_csv,"w",encoding="utf8",newline="\n") as f:
-    writer =csv.writer(f)
-    writer.writerow(ublisher_location)
+
